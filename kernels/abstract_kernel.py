9b2fc230a6791682c96d5524fe9cd7531cb9dc4b
Pablo A Rolandi <p.a.rolandi.aux@gmail.com>
2017-07-04 07:14:21 -0400

    Add kernels module.

---
+++ kernels/abstract_kernel.py
@@ -0,0 +1,21 @@
+import abc
+
+
+class AbstractKernelFactory(abc.ABC):
+    """
+    Its single responsibility is to create a "package" that offers a modeller-solver
+    combo of a concrete type (e.g., NLA)
+    """
+
+    def __init__(self, func):
+        """
+        :param func: the function that will be used by the modeller
+        """
+        self._func = func
+
+    @abc.abstractmethod
+    def create_kernel_package(self):
+        """
+        The main responsibility
+        :return: an AbstractModeller and AbstractSolver
+        """
---
+++ kernels/kernel.py
@@ -0,0 +1,33 @@
+import kernels.abstract_kernel as krn
+import modellers.nla_modeller as mdr
+import solvers.nla_solver as slv
+
+
+class ExplicitNlaKernelFactory(krn.AbstractKernelFactory):
+
+    def __init__(self, func):
+        super(ExplicitNlaKernelFactory, self).__init__(func)
+
+    def create_kernel_package(self):
+        modeller = mdr.ExplicitNlaModeller(self._func)
+        solver = slv.ExplicitNlaSolver(modeller)
+        return modeller, solver
+
+
+class Kernel():
+    """
+    As an abstraction, a kernel presents a complex systems of equations as a simple function evaluation
+    """
+
+    def __init__(self, factory):
+        self._modeller, self._solver = factory.create_kernel_package()
+
+    def set_values(self, x):
+        self._modeller.set_variable_values(x)
+
+    def compute(self):
+        self._solver.solve()
+
+    def get_solution(self):
+        return self._solver.get_solution()
+
