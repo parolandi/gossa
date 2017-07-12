b051273de1025ee5c7d742fd7920942bf1afcafe
Pablo A Rolandi <p.a.rolandi.aux@gmail.com>
2017-07-11 22:28:53 -0400

    #2. Test study with two contexts.

--- tests/test_nla_study.py
+++ tests/test_nla_study.py
@@ -10,12 +10,20 @@ class TestStudies(unittest.TestCase):
         aproblem = {"input_factors": ["x1", "x2"],
                  "output_responses": ["y1"],
                  "context": {"x1": 0, "x2": 1.1}}
+
         afactory = sdy.ExplicitNlaStudyFactory(mock.sum_twice_sum, aproblem)
         astudy = sdy.Study(afactory)
         astudy.set_context()
         astudy.execute()
         actual = astudy.get_solution()
         expected = [1.1, 2.2]
+        [self.assertAlmostEqual(left, right, delta = 1e-9) for left, right in zip(actual, expected)]
+
+        aproblem["context"]["x1"] = 1
+        astudy.set_context()
+        astudy.execute()
+        actual = astudy.get_solution()
+        expected = [2.1, 4.2]
         [self.assertAlmostEqual(left, right, delta = 1e-9) for left, right in zip(actual, expected)]
 
 
